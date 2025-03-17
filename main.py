95865eee30cfe06d5efc543ff5223f34a0ead1ad
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2025-03-17 01:36:22 +0000

    Mar 17, 2025, 1:36 AM

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-# testing.
-print("hello")
-# more testing
-
-print("testing")
-
-print("helllooooo")
-
-print("yoyooyo")
-
-print("testing")
-
-### hello
-
-
-print("test")
-
-# 123 542345 23
+# testing
