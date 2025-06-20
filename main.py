6e7495daf499825da3b104f1ddfceebba63f7328
ynandwani <ynandwani@kellettschool.com>
2025-06-20 01:18:54 +0000

    Jun 20, 2025, 1:18 AM

--- main.py
+++ main.py
@@ -37,19 +37,19 @@ def deal_cards_start(deck):
         card_1 = 10
     elif card_1 =='ace':
         card_1 = 11
-        hand[0].append[card_1]
+        hand[0] = card_1
         
     else:
-        hand[0].append(card_1)
+        hand[0] = card_1
     
     if card_2 == 'king' or card_2 == 'queen' or card_2 == 'jack':
         card_2 = 10
     elif card_2 =='ace':
         card_2 = 11
-        hand[1].append(card_2)
+        hand[1] = card_2
         
     else:
-        hand.append(card_2)
+        hand[1] = card_2
 
 
     print(f"Your hand is: {hand}")
