4f676d40a63361395ec8632b7d13f65e008a8d0c
ynandwani <ynandwani@kellettschool.com>
2025-06-23 03:47:16 +0000

    finished the game for hwk,working to implement revision aspect

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from random import randint
-from time import sleep
+import random
 import os
+from time import sleep
+
+welcome_message = "Welcome to Blackjack! "
+i = 0
+for i in range(len(welcome_message)):
+    print(welcome_message[i], end = ' ') 
+    i += 1
+    sleep(0.1)
+print('\n')
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -62,13 +71,16 @@ def double_down(hand, deck, player):
     return hand, deck
 
 def split_hand(hand, deck, player):
-    print(f"{player} split the hand.")
-    hand_1 = [hand[0], deck.pop(randint(0, len(deck) - 1))]
-    hand_2 = [hand[1], deck.pop(randint(0, len(deck) - 1))]
-    
-    print(f"{player}'s hands after split: {hand_1} and {hand_2}")
-    
-    return hand_1, hand_2, deck
+    if hand[0] == hand[1]:
+        print(f"{player} split the hand.")
+        hand_1 = [hand[0], deck.pop(randint(0, len(deck) - 1))]
+        hand_2 = [hand[1], deck.pop(randint(0, len(deck) - 1))]
+        
+        print(f"{player}'s hands after split: {hand_1} and {hand_2}")
+        return [hand_1], [hand_2], deck
+    else:
+        print("You can only split if you have a pair of cards with the same value.")
+        return [hand], [], deck
 
 def player_bust():
     print("You busted! Dealer wins.")
@@ -76,61 +88,61 @@ def player_bust():
 def dealer_bust():
     print("Dealer busted! You win.")
 
-def who_won(dealer_hand_total, hand_total, bet):
+def who_won(dealer_hand_total, hand_total):
     if hand_total > 21:
         print("You busted! Dealer wins.")
-        return -bet
+        question()
     elif dealer_hand_total > 21:
         print("Dealer busted! You win.")
-        return bet
     elif hand_total > dealer_hand_total:
         print("You win!")
-        return bet
     elif hand_total < dealer_hand_total:
         print("Dealer wins!")
-        return -bet
+        question()
     else:
         print("It's a tie.")
-        return 0
 
-def betting():
-    num_questions = int(input("How many math questions do you want to answer to determine your bet? "))
-    bet = 0
-    for _ in range(num_questions):
-        num1 = randint(1, 10)
-        num2 = randint(1, 10)
-        correct_answer = num1 + num2
-        answer = int(input(f"What is the sum of {num1} and {num2}? "))
-        if answer == correct_answer:
-            bet += 1
-        else:
-            break
-    return bet
+    play_again = input("Do you want to play again?(Y/N)").lower()
+    if play_again =='y':
+        clear_terminal()
+        start()
+    else:
+        print("Ok, bye!")
+
+def question():
+    directory = ""
+    files = [f for f in os.listdir(directory)]
+    random_file = random.choice(files)
+    file_path = os.path.join(directory, random_file)
+    
+    # Open the random file
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(f"Contents of {random_file}:\n")
+        print(content)
 
 def start():
-    balance = 100
-    play = input("Press Y to play. Otherwise, press N.").lower()
-    while play == 'y' and balance > 0:
-        bet = betting()
-        deck = cards()
-        player_hand, deck = deal_cards_start(deck)
-        dealer_hand = []
 
-        player_total = count_cards(player_hand, "You")
-        
-        while player_total <= 21:
-            action = input("Do you want to hit, stand, double down, or split? (hit/stand/double/split)").lower()
-            if action == 'hit':
-                player_hand, deck = hit(player_hand, deck, "You")
-                player_total = count_cards(player_hand, "You")
-            elif action == 'stand':
-                break
-            elif action == 'double':
-                player_hand, deck = double_down(player_hand, deck, "You")
-                player_total = count_cards(player_hand, "You")
-                bet *= 2
-            elif action == 'split' and player_hand[0] == player_hand[1]:
+    deck = cards()
+    player_hand, deck = deal_cards_start(deck)
+    dealer_hand = []
+
+    player_total = count_cards(player_hand, "You")
+    
+    while player_total <= 21:
+        action = input("Do you want to hit, stand, double, or split? (hit/stand/double/split)").lower()
+        if action == 'hit':
+            player_hand, deck = hit(player_hand, deck, "You")
+            player_total = count_cards(player_hand, "You")
+        elif action == 'stand':
+            break
+        elif action == 'double':
+            player_hand, deck = double_down(player_hand, deck, "You")
+            player_total = count_cards(player_hand, "You")
+        elif action == 'split':
+            if player_hand[0] == player_hand[1]:
                 hand_1, hand_2, deck = split_hand(player_hand, deck, "You")
+                
                 player_total_1 = count_cards(hand_1, "Hand 1")
                 player_total_2 = count_cards(hand_2, "Hand 2")
                 
@@ -147,25 +159,19 @@ def start():
                     action_2 = input("Do you want to hit or stand on Hand 2? (hit/stand)").lower()
 
                 break
+            else:
+                print("You can only split if you have a pair of cards with the same value.")
 
-        if player_total > 21:
-            balance -= bet
-        else:
+    if player_total <= 21:
+        dealer_total = count_cards(dealer_hand, "Dealer")
+
+        while dealer_total < 17:
+            dealer_hand, deck = hit(dealer_hand, deck, "Dealer")
             dealer_total = count_cards(dealer_hand, "Dealer")
+            if dealer_total > 21:
+                break
 
-            if player_total <= 21:
-                while dealer_total < 17:
-                    dealer_hand, deck = hit(dealer_hand, deck, "Dealer")
-                    dealer_total = count_cards(dealer_hand, "Dealer")
-                    if dealer_total > 21:
-                        break
-
-            balance += who_won(dealer_total, player_total, bet)
-
-        print(f"Your balance: {balance}")
-        play = input("Press Y to play again. Otherwise, press N.").lower()
-
-    print("Game over! Thanks for playing.")
+    who_won(dealer_total, player_total)
 
 if __name__ == "__main__":
     start()
